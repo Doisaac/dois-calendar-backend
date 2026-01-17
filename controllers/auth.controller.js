d179f74458dae15ff523e2085b7c1418dd414493
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-01-16 21:16:24 -0600

    feat: implement user registration with password encryption

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,42 @@
 import { response } from 'express'
+import bcrypt from 'bcryptjs'
 
-export const createUser = (req, res = response) => {
-  const { name, email, password } = req.body || {}
+import User from '../models/User.js'
 
-  res.status(201).json({
-    ok: true,
-    msg: 'login',
-    name,
-    email,
-    password,
-  })
+export const createUser = async (req, res = response) => {
+  const { email, password } = req.body || {}
+
+  try {
+    let user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'A user with that email already exists',
+      })
+    }
+
+    user = new User(req.body)
+
+    // Encrypt password
+    const salt = bcrypt.genSaltSync()
+    user.password = bcrypt.hashSync(password, salt)
+
+    await user.save()
+
+    res.status(201).json({
+      ok: true,
+      uui: user.id,
+      name: user.name,
+    })
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Please contact an administrator',
+    })
+  }
 }
 
 export const loginUser = (req, res = response) => {
