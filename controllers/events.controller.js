2ccdd7bbf7dbf1bbeb569e3dfded088bf300d64e
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-01-19 16:32:43 -0600

    feat: implement deleteEvent functionality with validation and permission checks

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -81,9 +81,40 @@ export const updateEvent = async (req, res = response) => {
   }
 }
 
-export const deleteEvent = (req, res = response) => {
-  return res.status(200).json({
-    ok: true,
-    msg: 'deleteEvent',
-  })
+export const deleteEvent = async (req, res = response) => {
+  const userId = req.uuid
+  const eventId = req.params.id
+
+  try {
+    const event = await Event.findById(eventId)
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'No event was found with that id',
+      })
+    }
+
+    // Validate if the requester is the user who created the event
+    if (event.user.toString() !== userId) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'You do not have permission to edit this event',
+      })
+    }
+
+    const deletedEvent = await Event.findByIdAndDelete(eventId)
+
+    return res.status(200).json({
+      ok: true,
+      msg: 'Event successfully deleted',
+      event: deletedEvent,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Please contact an administrator',
+    })
+  }
 }
